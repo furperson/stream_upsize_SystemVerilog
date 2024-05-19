35637292779cc2832998e87d7a10303adff16133
furperson <samnsk2606@gmail.com>
2024-05-19 22:23:11 +0700

    change some code

--- top.sv
+++ top.sv
@@ -15,23 +15,39 @@ module stream_upsize #(
     input   logic                       m_ready_i
 );
 
-sync_fifo fifo();// fifo for saving temp date of transaction 
+
+//fifo var section
+logic wr_en = 0 ;
+logic rd_en = 0;
+logic f_din;
+logic f_dout;
+logic empty;
+logic full;
+sync_fifo #(DWIDTH= , DEPTH=8 ) fifo ( .rstn(rst_n),
+                         .wr_en(wr_en),
+                         .rd_en(rd_en),
+                         .wr_clk(clk),
+                         .rd_clk(clk),
+                         .din(f_din),
+                         .dout(f_dout),
+                         .empty(empty),
+                         .full(full)
+                        );
+// end fifo section
 
 logic [T_DATA_WIDTH-1:0] inner_trans [T_DATA_RATIO];
-logic [T_DATA_WIDTH-1:0] in_data = b0;
+logic [T_DATA_WIDTH-1:0] in_data = 0;
 logic counter ;
-always @(posedge clk and s_valid_i) begin
+
+always @(posedge clk and s_valid_i and !full)  begin // block - reading packets
     if(rst_n)
     else begin 
         s_ready_o <= 1'b1;
         in_data <= s_data_i;
-        
-
-
     end
 end
 
-always @(posedge clk) begin
+always @(posedge clk and ) begin
 
     
 end
