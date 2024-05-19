f9a7769be6475db0fdf065b27efafcb3f1d02498
furperson <samnsk2606@gmail.com>
2024-05-19 13:05:04 +0700

    add main code and fifo module

---
+++ top.sv
@@ -0,0 +1,35 @@
+module stream_upsize #(
+    parameter T_DATA_WIDTH = 1 ,
+    T_DATA_RATIO =2
+)(
+    input   logic                       clk,
+    input   logic                       rst_n,
+    input   logic [T_DATA_WIDTH-1:0]    s_data_i,
+    input   logic                       s_last_i,
+    input   logic                       s_valid_i,
+    output  logic                       s_ready_o,
+    output  logic [T_DATA_WIDTH-1:0]    m_data_o   [T_DATA_RATIO],
+    output  logic [T_DATA_RATIO-1:0]    m_keep_o,
+    output  logic                       m_last_o,
+    output  logic                       m_valid_o,
+    input   logic                       m_ready_i
+);
+
+logic [T_DATA_WIDTH-1:0] inner_trans [T_DATA_RATIO]
+always @(posedge clk and s_valid_i) begin
+    if(rst_n)
+    else begin 
+        s_ready_o <= 1'b1;
+
+
+
+    end
+end
+
+always @(posedge clk) begin
+
+    
+end
+
+
+endmodule
