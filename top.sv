ba34e2b9e27e9e62b7f3d63f119eadcbeb6671aa
furperson <samnsk2606@gmail.com>
2024-05-20 08:56:09 +0700

    nothing

--- top.sv
+++ top.sv
@@ -37,15 +37,34 @@ sync_fifo #(DWIDTH= T_DATA_RATIO*T_DATA_WIDTH + T_DATA_RATIO, DEPTH=8 ) fifo ( .
 
 logic [T_DATA_WIDTH-1:0] inner_trans [T_DATA_RATIO];
 logic [T_DATA_RATIO] counter = 0; //counter for packet output
+logic data_on_out =0;// flag about exist out data 
 
-always @(posedge clk and s_valid_i and )  begin // block - reading packets
-    if(rst_n)
-    else begin 
-
+always @(posedge clk and s_valid_i==1 )  begin // block - reading packets
+    if(!rst_n)  begin 
+        if(counter==T_DATA_RATIO) s_ready_o <=1'b0;
+        else begin
+        s_ready_o<=1'b1;
+        counter<= counter +1;
+        inner_trans [counter-1]<=s_data_i;
+        end
     end
 end
 
-always @(posedge clk and ) begin //block - output transaction 
+
+
+always @(posedge clk and (counter == T_DATA_RATIO or s_last_i==1)) begin //block - output transaction 
+    if(!data_on_out) begin
+        genvar i;
+        generate
+            for(i=0;i<T_DATA_RATIO;i++) begin
+                m_data_o<=inner_trans
+            end
+        endgenerate
+    end
+end
+
+
+always @(posedge clk and(counter == ) ) begin 
 
     
 end
